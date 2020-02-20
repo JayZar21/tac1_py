3d30ce60f5769cf80d46b0e23838f06a442e799c
Gianluca Romanin <gianluca.romanin@vimar.com>
2020-02-20 11:45:07 +0100

    added time in the note and basic widget

--- tac1.py
+++ tac1.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import datetime
 import npyscreen
 from configparser import ConfigParser
+from collections import namedtuple
 
-DEFAULT_DATA_PATH = "~/.tac1"
+DEFAULT_DATA_PATH = os.path.expanduser("~/.tac1")
 
 
 class TempDict(dict):
@@ -17,57 +19,65 @@ class Notebook():
 
     def __init__(self, path=DEFAULT_DATA_PATH):
         self.notes = dict()
-        os.makedirs(path,exist_ok=True)
+        os.makedirs(path, exist_ok=True)
         for root, dirs, files in os.walk(path):
             for file in files:
                 note_path = root + os.sep + file
                 n = Note(note_path)
                 self.notes[n.title] = n
-        print(self.notes)
+        print(self.notes.keys())
                 
 
 NOTE_CONTENT_TPL = \
 """[title]: {TITLE}
 [tags]: {TAG_LIST}
+[time]: {TIME}
 
 {CONTENT}
 """
 class Note():
 
-    
-
     def __init__(self, path):
         self.title = ""
         self.tags = list()
+        self.time = ""
         self.content = ""
         print(path)
         with open(path, "r") as fr:
             content = fr.readlines()
+            print(content)
             if len(content) > 2:
                 self.title = content[0].replace("[title]: ", "").replace("\n", "")
                 self.tags = content[1].replace("[tags]: ", "").replace("\n", "").split(" ")
-                self.content = content[2:]
+                self.time = content[2].replace("[time]: ", "").replace("\n", "")
+                self.content = content[3:]
 
     @classmethod
-    def format_note(cls, title, tag_list, content):
+    def format_note(cls, title, tag_list, time, content):
         params = \
         {
             "TITLE" : title,
             "TAG_LIST" : tag_list,
+            "TIME" : time,
             "CONTENT" : content,
         }
         return format_tpl(NOTE_CONTENT_TPL, params)
 
+NOTE_BOOK = Notebook()
 
 class TacApplication(npyscreen.NPSAppManaged):
     def onStart(self):
-        #self.myDatabase = AddressDatabase()
-        self.addForm("MAIN", MainForm)
-        #self.addForm("EDITRECORDFM", EditRecord)
+        self.addForm("MAIN", NotesGrid)
 
-class MainForm(npyscreen.Form):
+class NotesGrid(npyscreen.Form):
     def create(self):
-        self.add(npyscreen.TitleText, name = "Text:", value= "Hellow World!" )
+        # NOT WORKING WELL
+        #grid = self.add(npyscreen.GridColTitles, relx = 5, rely=5, col_titles = [''])
+        #grid.values = [ (n.time + " | " + n.title,) for n in NOTE_BOOK.notes.values()]
+
+        nb = self.add(npyscreen.BoxTitle, name="Notes:")
+        nb.values = [ n.time + " | " + n.title for n in NOTE_BOOK.notes.values()]
+
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
--- test_tac1.py
+++ test_tac1.py
@@ -1,5 +1,6 @@
 
 import tempfile
+import datetime
 from tac1 import *
 
 
@@ -9,7 +10,7 @@ def test_notebook():
     for i in [1, 2, 3]:
         note = temp_folder + os.sep + str(i)
         with open(note, "w") as fw:
-            fw.write(Note.format_note(str(i), "A B C D", "TEST_NOTE"))
+            fw.write(Note.format_note(str(i), "A B C D", datetime.datetime.now().strftime("%H:%M %B %d, %Y"), "TEST_NOTE"))
     
     nb = Notebook(temp_folder)
     assert "B" in nb.notes["1"].tags
