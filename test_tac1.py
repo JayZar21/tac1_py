d992563b4731a46dfeb1079b74be68f80eadb37a
Gianluca Romanin <gianluca.romanin@vimar.com>
2020-02-25 17:38:20 +0100

    Initial move to asciimatics

--- test_tac1.py
+++ test_tac1.py
@@ -8,9 +8,9 @@ from tac1 import *
 def test_notebook():
     temp_folder = tempfile.mkdtemp()
     for i in [1, 2, 3]:
-        note = temp_folder + os.sep + str(i)
-        with open(note, "w") as fw:
-            fw.write(Note.format_note(str(i), "A B C D", datetime.datetime.now().strftime("%H:%M %B %d, %Y"), "TEST_NOTE"))
-    
+        note_path = temp_folder + os.sep + str(i)
+        n = Note(note_path, str(i), "A B C D",  "TEST_NOTE")
+        n.write()
+
     nb = Notebook(temp_folder)
     assert "B" in nb.notes["1"].tags
